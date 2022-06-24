73ece956b55d6c2d6611d277d822186dbd029312
hagbardlol <59706487+hagbardlol@users.noreply.github.com>
2022-06-24 03:20:50 -0800

    GitHub Link Update github.com To raw.githubusercontent.com

--- AuLissandra.lua
+++ AuLissandra.lua
@@ -5,7 +5,7 @@ local scriptCreator = "AURUM"
 local credits = "Orietto"
 local patchNotesPrevUpdate = "03/13/2022"
 local patchNotesPreVersion = "1.0.1"
-local patchNotesVersion, scriptVersionUpdater = "1.0.2", "1.0.2"
+local patchNotesVersion, scriptVersionUpdater = "1.0.3", "1.0.3"
 local scriptVersion = scriptVersionUpdater
 local scriptLastUpdated = "03/14/2022"
 local scriptIsBeta = false
@@ -26,7 +26,7 @@ local huge, pow, min, max, floor = math.huge, math.pow, math.min, math.max, math
 
 local SDK = _G.CoreEx
 
-SDK.AutoUpdate("https://github.com/roburAURUM/robur-AuEdition/raw/main/AuLissandra.lua", scriptVersionUpdater)
+SDK.AutoUpdate("https://raw.githubusercontent.com/roburAURUM/robur-AuEdition/main/AuLissandra.lua", scriptVersionUpdater)
 
 local ObjManager = SDK.ObjectManager
 local EventManager = SDK.EventManager
